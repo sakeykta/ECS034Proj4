1d13334f7e85dc5723317b3fb57d5dd129756688
manik.sethi <66762318+manik-sethi@users.noreply.github.com>
2024-03-11 09:41:48 -0700

    Update DSVWriter.cpp

--- src/DSVWriter.cpp
+++ src/DSVWriter.cpp
@@ -1,6 +1,8 @@
 #include "DSVWriter.h"
 #include "StringUtils.h"
 #include "StringDataSink.h"
+#include <sstream>
+#include <iostream>
 
 // Implementation struct definition
 struct CDSVWriter::SImplementation {
@@ -9,56 +11,54 @@ struct CDSVWriter::SImplementation {
     bool DQuoteAll;
 
     SImplementation(std::shared_ptr<CDataSink> sink, char delimiter, bool quoteall)
-        : DDataSink(sink), DDelimiter(delimiter), DQuoteAll(quoteall) {
+        : DDataSink(sink), DDelimiter(delimiter), DQuoteAll(quoteall) {}
 
+    std::string QuoteIfNeeded(const std::string &field) {
+    if (DQuoteAll || field.find(DDelimiter) != std::string::npos || field.find('"') != std::string::npos || field.find('\n') != std::string::npos) {
+        std::string result = "\"";
+        for (char ch : field) {
+            if (ch == '"')
+                result += "\"\"";
+            else
+                result += ch;
+        }
+        result += "\"";
+        return result;
     }
+    return field;
+}
 };
 
 // Constructor
 CDSVWriter::CDSVWriter(std::shared_ptr<CDataSink> sink, char delimiter, bool quoteall)
-    : DImplementation(std::make_unique<SImplementation>(sink, delimiter, quoteall)) {
-
-}
+    : DImplementation(std::make_unique<SImplementation>(sink, delimiter, quoteall)) {}
 
 // Destructor
-CDSVWriter::~CDSVWriter() {
-
-}
+CDSVWriter::~CDSVWriter() = default;
 
 // WriteRow function
 bool CDSVWriter::WriteRow(const std::vector<std::string> &row) {
-
-    if (!DImplementation->DDataSink) {
-        return false;
-    }
-    std::string rowString;
-    bool quoteNeeded = DImplementation->DQuoteAll;
+    std::ostringstream oss;
     for (size_t i = 0; i < row.size(); ++i) {
+        oss << DImplementation->QuoteIfNeeded(row[i]);
+        if (i + 1 < row.size())
+            oss << DImplementation->DDelimiter;
+    }
+    // Append newline character after each row
+    oss << '\n';
 
-        if (i > 0) {
-            rowString += DImplementation->DDelimiter;
-        }
-        std::string field = row[i];
-        if (!DImplementation->DQuoteAll) {
-            if (field.find(DImplementation->DDelimiter) != std::string::npos ||
-                field.find('\n') != std::string::npos) {
-                quoteNeeded = true;
-            }
-        }
-        if (quoteNeeded) {
-            rowString += "\"" + field + "\"";
-        } else {
-            rowString += field;
-        }
-    }
-    std::vector<char> buffer(rowString.begin(), rowString.end());
-    if (!DImplementation->DDataSink->Write(buffer)) {
-        return false;
-    }
-    char newline = '\n';
-    if (!DImplementation->DDataSink->Write(std::vector<char>{newline})) {
-        return false; 
+    std::string str = oss.str();
+    std::cout << "Row data after building: " << oss.str() << std::endl;
+    // Remove the newline character at the end if it exists
+    if (!str.empty() && str.back() == '\n') {
+        str.pop_back();
     }
 
-    return true;
+    return DImplementation->DDataSink->Write(std::vector<char>(str.begin(), str.end()));
 }
+
+
+
+
+
+
